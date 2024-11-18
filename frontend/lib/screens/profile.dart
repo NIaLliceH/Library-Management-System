5d9a1495fdc3732ad7b58b7a09ad0384df9459a1
ngochoaphan2004 <phanngochoa2004@gmail.com>
2024-11-18 11:37:59 +0700

    update profile screen (change info)

--- frontend/lib/screens/profile.dart
+++ frontend/lib/screens/profile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/constants.dart';
 
+enum VariableID { name, id, dep, date, gen, join }
+
 //header
 class InvertedCornerClipper extends CustomClipper<Path> {
   @override
@@ -30,7 +32,11 @@ class InvertedCornerClipper extends CustomClipper<Path> {
 class _InfoElement extends StatefulWidget {
   final String title, content;
   final bool finalLine;
-  const _InfoElement(this.title, this.content, {this.finalLine = false});
+  final bool editState;
+  final VariableID state;
+  final Function? onUpdate;
+  const _InfoElement(this.title, this.content, this.editState, this.state,
+      {this.finalLine = false, this.onUpdate, super.key});
   @override
   State<StatefulWidget> createState() => _InfoState();
 }
@@ -42,7 +48,7 @@ class _InfoState extends State<_InfoElement> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 15),
           child: Text(
             widget.title,
             style: TextStyle(
@@ -51,30 +57,45 @@ class _InfoState extends State<_InfoElement> {
             ),
           ),
         ),
-        SizedBox(height: 3),
-        Row(children: [
-          Expanded(
-            child: Container(
-              decoration: widget.finalLine
-                  ? null
-                  : BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(width: 1, color: kBase5Color),
-                      ),
-                    ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                child: Text(
-                  widget.content,
-                  style: TextStyle(
+        // SizedBox(height: 3),
+        Container(
+          decoration: widget.finalLine
+              ? null
+              : BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(width: 1, color: kBase5Color),
+                  ),
+                ),
+          child: Theme(
+            data: ThemeData(
+              inputDecorationTheme: InputDecorationTheme(
+                isDense: true,
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: TextFormField(
+                readOnly: !widget.editState,
+                initialValue: widget.content,
+                onFieldSubmitted: (value) {
+                  widget.onUpdate!(widget.state, value);
+                },
+                decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                  hintText: widget.title,
+                  labelStyle: TextStyle(
                     fontSize: smallFontSize,
                     fontWeight: FontWeight.w400,
                   ),
+                  border: InputBorder.none,
                 ),
               ),
             ),
           ),
-        ]),
+        ),
       ],
     );
   }
@@ -95,6 +116,33 @@ class _ProfileScreenState extends State<ProfileScreen> {
       dob = "10/10/2004",
       gender = "Male",
       joinDate = "5/11/2024";
+  String tempName = "Phan Ngoc Hoa",
+      tempDepartment = "abc",
+      tempDob = "10/10/2004",
+      tempGender = "Male",
+      tempJoinDate = "5/11/2024";
+  bool editState = false;
+
+  void updateVariable(VariableID id, String newValue) {
+    switch (id) {
+      case VariableID.name:
+        tempName = newValue;
+        break;
+      case VariableID.date:
+        tempDob = newValue;
+        break;
+      case VariableID.dep:
+        tempDepartment = newValue;
+        break;
+      case VariableID.gen:
+        tempGender = newValue;
+        break;
+      case VariableID.join:
+        tempJoinDate = newValue;
+        break;
+      default:
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -191,27 +239,120 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         SizedBox(height: 10),
                         Container(
                           width: screensize - 50,
-                          padding: EdgeInsets.symmetric(vertical: 10),
+                          padding: EdgeInsets.only(top: 10),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(7),
                             color: Colors.white,
                           ),
                           child: Column(
-                            children: [
-                              _InfoElement("Department", department),
-                              SizedBox(height: 10),
-                              _InfoElement("Date of birth", dob),
-                              SizedBox(height: 10),
-                              _InfoElement("Gender", gender),
-                              SizedBox(height: 10),
-                              _InfoElement(
-                                "Join date",
-                                joinDate,
-                                finalLine: true,
-                              ),
-                            ],
+                            children: editState
+                                ? [
+                                    _InfoElement(
+                                      "Name",
+                                      name,
+                                      true,
+                                      VariableID.name,
+                                      onUpdate: updateVariable,
+                                      key: ValueKey("name"),
+                                    ),
+                                    SizedBox(height: 10),
+                                    _InfoElement(
+                                      "Department",
+                                      department,
+                                      true,
+                                      VariableID.dep,
+                                      onUpdate: updateVariable,
+                                      key: ValueKey("dep"),
+                                    ),
+                                    SizedBox(height: 10),
+                                    _InfoElement(
+                                      "Date of birth",
+                                      dob,
+                                      true,
+                                      VariableID.date,
+                                      onUpdate: updateVariable,
+                                      key: ValueKey("date"),
+                                    ),
+                                    SizedBox(height: 10),
+                                    _InfoElement(
+                                      "Gender",
+                                      gender,
+                                      true,
+                                      VariableID.gen,
+                                      onUpdate: updateVariable,
+                                      key: ValueKey("gen"),
+                                    ),
+                                    SizedBox(height: 10),
+                                    _InfoElement(
+                                      "Join date",
+                                      joinDate,
+                                      true,
+                                      VariableID.join,
+                                      onUpdate: updateVariable,
+                                      finalLine: true,
+                                      key: ValueKey("join"),
+                                    ),
+                                  ]
+                                : [
+                                    _InfoElement(
+                                      "Department",
+                                      department,
+                                      false,
+                                      VariableID.name,
+                                    ),
+                                    SizedBox(height: 10),
+                                    _InfoElement(
+                                      "Date of birth",
+                                      dob,
+                                      false,
+                                      VariableID.date,
+                                    ),
+                                    SizedBox(height: 10),
+                                    _InfoElement(
+                                      "Gender",
+                                      gender,
+                                      false,
+                                      VariableID.gen,
+                                    ),
+                                    SizedBox(height: 10),
+                                    _InfoElement(
+                                      "Join date",
+                                      joinDate,
+                                      false,
+                                      VariableID.join,
+                                      finalLine: true,
+                                    ),
+                                  ],
                           ),
-                        )
+                        ),
+                        SizedBox(height: 5),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              if (editState) {
+                                name = tempName;
+                                department = tempDepartment;
+                                dob = tempDob;
+                                gender = tempGender;
+                                joinDate = tempJoinDate;
+                              }
+                              editState = !editState;
+                              print(editState);
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: editState ? Colors.yellow : kBase2Color,
+                            ),
+                            width: screensize - 50,
+                            height: 40,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(editState ? "Done" : "Edit Profile"),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
